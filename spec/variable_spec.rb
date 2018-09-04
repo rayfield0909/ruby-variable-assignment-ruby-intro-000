e8669de7618b14036b86313515dcda219ac652c6
raymond.g.field@gmail.com <raymond.g.field@gmail.com>
2018-09-04 15:21:42 +0000

    automatically backed up by learn

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,6 +1,7 @@
-# Code your solution in varia
-describe "./variable.rb" do
 greeting = "Hello World"
+
+describe "./variable.rb" do
+
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
     greeting = get_variable_from_file('./variable.rb', "greeting")
 
